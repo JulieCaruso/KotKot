a178fdef006d647896cc93851486ab3258e36a2c
Julie CARUSO <jcaruso@adneom.com>
2017-08-17 15:37:34 +0200

    Add message

--- app/src/main/java/com/application/kotkot/MainActivity.kt
+++ app/src/main/java/com/application/kotkot/MainActivity.kt
@@ -3,11 +3,15 @@ package com.application.kotkot
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+import kotlinx.android.synthetic.main.activity_main.*;
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        text.setOnClickListener( {
+            text.setText(text.text.toString() + " again")
+        })
     }
 }
